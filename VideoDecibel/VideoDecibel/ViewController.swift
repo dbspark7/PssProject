e04d5739dc7c5e21247b61933a607da4ed0a831f
박수성 <pss0711@truen.co.kr>
2022-11-28 16:54:09 +0900

    WIP

--- VideoDecibel/VideoDecibel/ViewController.swift
+++ VideoDecibel/VideoDecibel/ViewController.swift
@@ -8,8 +8,12 @@
 import UIKit
 import AVFoundation
 import Photos
+import Charts
 
 class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate, AVCaptureAudioDataOutputSampleBufferDelegate {
+    
+    @IBOutlet var videoView: UIView!
+    @IBOutlet var chartView: LineChartView!
     
     var captureSession: AVCaptureSession!
     var backCamera: AVCaptureDevice!
@@ -24,6 +28,8 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
     var isBackCamera = true
     
     var recorder: AVAudioRecorder!
+    
+    var shouldHideData: Bool = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -79,9 +85,140 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         previewLayer = AVCaptureVideoPreviewLayer(session: captureSession)
         previewLayer.videoGravity = .resizeAspectFill
         previewLayer.connection?.videoOrientation = .portrait
-        self.previewLayer.frame = self.view.frame
-        view.layer.insertSublayer(previewLayer, at: 0)
+        self.previewLayer.frame = self.videoView.frame
+        self.videoView.layer.insertSublayer(previewLayer, at: 0)
         
+        
+        chartView.delegate = self
+
+        chartView.chartDescription.enabled = false
+        chartView.dragEnabled = true
+        chartView.setScaleEnabled(true)
+        chartView.pinchZoomEnabled = true
+        
+        // x-axis limit line
+        let llXAxis = ChartLimitLine(limit: 10, label: "Index 10")
+        llXAxis.lineWidth = 4
+        llXAxis.lineDashLengths = [10, 10, 0]
+        llXAxis.labelPosition = .rightBottom
+        llXAxis.valueFont = .systemFont(ofSize: 10)
+
+        chartView.xAxis.gridLineDashLengths = [10, 10]
+        chartView.xAxis.gridLineDashPhase = 0
+
+        let ll1 = ChartLimitLine(limit: 150, label: "Upper Limit")
+        ll1.lineWidth = 4
+        ll1.lineDashLengths = [5, 5]
+        ll1.labelPosition = .rightTop
+        ll1.valueFont = .systemFont(ofSize: 10)
+
+        let ll2 = ChartLimitLine(limit: -30, label: "Lower Limit")
+        ll2.lineWidth = 4
+        ll2.lineDashLengths = [5,5]
+        ll2.labelPosition = .rightBottom
+        ll2.valueFont = .systemFont(ofSize: 10)
+
+        let leftAxis = chartView.leftAxis
+        leftAxis.removeAllLimitLines()
+        leftAxis.addLimitLine(ll1)
+        leftAxis.addLimitLine(ll2)
+        leftAxis.axisMaximum = 50
+        leftAxis.axisMinimum = -150
+        leftAxis.gridLineDashLengths = [5, 5]
+        leftAxis.drawLimitLinesBehindDataEnabled = true
+
+        chartView.rightAxis.enabled = false
+        
+//        let marker = BalloonMarker(color: UIColor(white: 180/255, alpha: 1),
+//                                   font: .systemFont(ofSize: 12),
+//                                   textColor: .white,
+//                                   insets: UIEdgeInsets(top: 8, left: 8, bottom: 20, right: 8))
+//        marker.chartView = chartView
+//        marker.minimumSize = CGSize(width: 80, height: 40)
+//        chartView.marker = marker
+
+        chartView.legend.form = .line
+        
+//        self.setDataCount(45, range: 100)
+        
+//        chartView.animate(xAxisDuration: 2.5)
+    }
+    
+    var values = [ChartDataEntry]()
+    
+    func setData() {
+        let set1 = LineChartDataSet(entries: values, label: "DataSet 1")
+        set1.drawIconsEnabled = false
+        setup(set1)
+
+        let value = ChartDataEntry(x: Double(3), y: 3)
+        set1.addEntryOrdered(value)
+        let gradientColors = [ChartColorTemplates.colorFromString("#00ff0000").cgColor,
+                              ChartColorTemplates.colorFromString("#ffff0000").cgColor]
+        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+
+        set1.fillAlpha = 1
+        set1.fill = LinearGradientFill(gradient: gradient, angle: 90)
+        set1.drawFilledEnabled = true
+
+        let data = LineChartData(dataSet: set1)
+
+        chartView.data = data
+    }
+    
+//    func setDataCount(_ count: Int, range: UInt32) {
+//        let values = (0..<count).map { (i) -> ChartDataEntry in
+//            let val = Double(arc4random_uniform(range) + 3)
+//            return ChartDataEntry(x: Double(i), y: val, icon: nil)
+//        }
+//
+//        let set1 = LineChartDataSet(entries: values, label: "DataSet 1")
+//        set1.drawIconsEnabled = false
+//        setup(set1)
+//
+//        let value = ChartDataEntry(x: Double(3), y: 3)
+//        set1.addEntryOrdered(value)
+//        let gradientColors = [ChartColorTemplates.colorFromString("#00ff0000").cgColor,
+//                              ChartColorTemplates.colorFromString("#ffff0000").cgColor]
+//        let gradient = CGGradient(colorsSpace: nil, colors: gradientColors as CFArray, locations: nil)!
+//
+//        set1.fillAlpha = 1
+//        set1.fill = LinearGradientFill(gradient: gradient, angle: 90)
+//        set1.drawFilledEnabled = true
+//
+//        let data = LineChartData(dataSet: set1)
+//
+//        chartView.data = data
+//    }
+    
+    private func setup(_ dataSet: LineChartDataSet) {
+        if dataSet.isDrawLineWithGradientEnabled {
+            dataSet.lineDashLengths = nil
+            dataSet.highlightLineDashLengths = nil
+            dataSet.setColors(.black, .red, .white)
+            dataSet.setCircleColor(.black)
+            dataSet.gradientPositions = [0, 40, 100]
+            dataSet.lineWidth = 1
+            dataSet.circleRadius = 3
+            dataSet.drawCircleHoleEnabled = false
+            dataSet.valueFont = .systemFont(ofSize: 9)
+            dataSet.formLineDashLengths = nil
+            dataSet.formLineWidth = 1
+            dataSet.formSize = 15
+        } else {
+            dataSet.lineDashLengths = [5, 2.5]
+            dataSet.highlightLineDashLengths = [5, 2.5]
+            dataSet.setColor(.black)
+            dataSet.setCircleColor(.black)
+            dataSet.gradientPositions = nil
+            dataSet.lineWidth = 1
+            dataSet.circleRadius = 3
+            dataSet.drawCircleHoleEnabled = false
+            dataSet.valueFont = .systemFont(ofSize: 9)
+            dataSet.formLineDashLengths = [5, 2.5]
+            dataSet.formLineWidth = 1
+            dataSet.formSize = 15
+        }
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -166,6 +303,12 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
         
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        self.previewLayer.frame = self.videoView.frame
+    }
+    
     private func normalizeSoundLevel(level: Float) -> Float {
             // 화면에 표시되는 rawSoundLevel 기준
             // white noise만 존재할 때의 값을 lowLevel 에 할당
@@ -180,12 +323,23 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
             return level / (highLevel - lowLevel) // scaled to 0.0 ~ 1
         }
     
+    var count = 0
+    
     func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
 //        print("여기 \(output.connection(with: .video)) \(output.connection(with: .audio)))")
         if recorder != nil {
             recorder.updateMeters()
             let level = recorder.peakPower(forChannel: 0)
-            print("여기 \(recorder.isRecording) \(recorder.averagePower(forChannel: 0)), \(recorder.peakPower(forChannel: 0))")
+//            print("여기 \(recorder.isRecording) \(recorder.averagePower(forChannel: 0)), \(recorder.peakPower(forChannel: 0))")
+//            setDataCount(count, range: UInt32(recorder.peakPower(forChannel: 0)))
+            values.append(ChartDataEntry(x: Double(count), y: Double(level), icon: nil))
+            
+            count += 1
+            
+            DispatchQueue.main.async {
+                self.setData()
+            }
+            
 //            print("\(normalizeSoundLevel(level: level)) dB")
         }
         
@@ -228,4 +382,30 @@ class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDele
 //
 //            switchCameraButton.isUserInteractionEnabled = true
 //    }
+}
+
+extension ViewController: ChartViewDelegate {
+    func chartValueSelected(_ chartView: ChartViewBase, entry: ChartDataEntry, highlight: Highlight) {
+        
+    }
+    
+    func chartViewDidEndPanning(_ chartView: ChartViewBase) {
+        
+    }
+    
+    func chartValueNothingSelected(_ chartView: ChartViewBase) {
+        
+    }
+    
+    func chartScaled(_ chartView: ChartViewBase, scaleX: CGFloat, scaleY: CGFloat) {
+        
+    }
+    
+    func chartTranslated(_ chartView: ChartViewBase, dX: CGFloat, dY: CGFloat) {
+        
+    }
+    
+    func chartView(_ chartView: ChartViewBase, animatorDidStop animator: Animator) {
+        
+    }
 }
